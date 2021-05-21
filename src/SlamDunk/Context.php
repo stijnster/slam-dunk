f566cdf4ae3c2c43f2807084314be64da117d7b2
Stijn Mathysen <stijn@skylight.be>
2021-05-21 18:38:12 +0200

    start prepping for taskloader

--- src/SlamDunk/Context.php
+++ src/SlamDunk/Context.php
@@ -6,6 +6,7 @@ class Context {
 
 	private $_rootPath;
 	private $_arguments;
+	private $_tasksPath;
 	private $_tasks;
 
 	public function __construct(string $rootPath, array $arguments = []){
@@ -14,14 +15,30 @@ class Context {
 		}
 		$this->_rootPath = $rootPath;
 		$this->_arguments = new Arguments($arguments);
-		$this->_tasks = [];
+		$this->_tasksPaths = [];
+
+		$this->appendTaskPath(FileSystem\Directory::join([ 'tasks' ], [ 'basePath' => __DIR__.DIRECTORY_SEPARATOR.'..' ]));
 	}
 
 	public function getArguments() : Arguments {
 		return $this->_arguments;
 	}
 
-	public function getTasks() : array {
+	public function appendTaskPath(string $path){
+		if(FileSystem\Directory::exists($path)){
+			array_push($this->_tasksPaths, $path);
+		}
+	}
+
+	public function getTasksPaths() : array {
+		return $this->_tasksPaths;
+	}
+
+	public function getTasks(){
+		if($this->_tasks === NULL){
+			$this->_tasks = [];
+		}
+
 		return $this->_tasks;
 	}
 
--- tests/SlamDunk/ContextTest.php
+++ tests/SlamDunk/ContextTest.php
@@ -6,6 +6,7 @@ class SlamDunkContextTest extends \PHPUnit\Framework\TestCase{
 		$context = new \SlamDunk\Context(_ROOT_PATH);
 		$this->assertInstanceOf('\\SlamDunk\\Context', $context);
 		$this->assertInstanceOf('\\SlamDunk\\Arguments', $context->getArguments());
+		$this->assertEquals([ _ROOT_PATH.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'tasks' ], $context->getTasksPaths());
 		$this->assertEquals([], $context->getTasks());
 	}
 
